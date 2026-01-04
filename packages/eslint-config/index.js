535c57045c06186a4eea43b47d750aed3d085ba4
waran <114287960+CodeWaran@users.noreply.github.com>
2026-01-04 16:17:38 +0530

    Turbo mono-repo react projects configuration updated

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+    'react/prop-types': 'off', // Optional, if you use TypeScript
+  },
+};
